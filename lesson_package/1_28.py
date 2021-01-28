4a02722a7dc1d06434c7d98729d93716bf574d25
junoym <junnyaooyama@gmail.com>
2021-01-29 02:16:25 +0900

    五目並べ縦バージョン問題実装

--- lesson_package/1_28.py
+++ lesson_package/1_28.py
@@ -18,3 +18,32 @@ def calc(array, condition):
 
 output = calc(temperatures, 30)
 print(output)
+
+# C問題五目並べ縦で揃った方のプレイヤーを出力する
+board = []
+result = 'D'
+
+for i in range(5):
+    board.append(input())
+
+for i in range(5):
+    pivot = ''
+    count = 0
+
+    for j in range(5):
+        stone = board[j][i]
+
+        if pivot == '':
+            pivot = stone
+
+        if stone != '.' and stone == pivot:
+            count += 1
+        else:
+            break
+
+    if count == 5:
+        result = pivot
+        break
+
+
+print(result)
