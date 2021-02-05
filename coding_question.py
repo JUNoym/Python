0bd50a948a7d2d56d31e5d0154afe3c24ec461ef
junoym <junnyaooyama@gmail.com>
2021-02-05 23:50:48 +0900

    占い問題

--- coding_question.py
+++ coding_question.py
@@ -37,3 +37,24 @@ for stone in board:
     if count == 5:
         result = target
 print(result)
+
+# C問題　占い
+n = int(input())
+users = {}
+for i in range(n):
+    tmp = input().split()
+
+    users[tmp[0]] = tmp[1]
+
+m = int(input())
+fortunes = {}
+for i in range(m):
+    tmp = input().split()
+
+    fortunes[tmp[0]] = tmp[1]
+
+for user, user_blood in users.items():
+    for blood, fortune in fortunes.items():
+        if user_blood == blood:
+            print(user + ' ' + fortune)
+            break
