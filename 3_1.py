d656effc3520dc806c75b7bc455393d7e6aa516b
junoym <junnyaooyama@gmail.com>
2021-03-01 00:38:53 +0900

    B問題

--- 3_1.py
+++ 3_1.py
@@ -1,3 +1,4 @@
+# マップ判定縦
 h, w = map(int, input().split())
 s = [list(input()) for i in range(h)]
 # print(s)
@@ -9,4 +10,16 @@ for y in range(h):
         # また取り出した値が右端で,その左つまりx - 1が#の場合も条件を満たす
         if x == 0 or s[y][x - 1] == '#':
             if x == w - 1 or s[y][x + 1] == '#':
+                print(y, x)
+
+
+# マップ判定横
+h, w = map(int, input().split())
+s = [list(input()) for i in range(h)]
+
+for y in range(h):
+    for x in range(w):
+        if y == 0 or s[y - 1][x] == "#":
+            # 上を判定
+            if y == h - 1 or s[y + 1][x] == "#":
                 print(y, x)
