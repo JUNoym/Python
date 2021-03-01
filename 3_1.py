591d39a377040682c8d1ca5fc397cda10456a7e5
junoym <junnyaooyama@gmail.com>
2021-03-01 23:48:57 +0900

    縦横判定座標出力

--- 3_1.py
+++ 3_1.py
@@ -23,3 +23,25 @@ for y in range(h):
             # 上を判定
             if y == h - 1 or s[y + 1][x] == "#":
                 print(y, x)
+
+
+# B問題　縦横判定座標出力
+
+h, w = map(int, input().split())
+s = [list(input()) for i in range(h)]
+
+for y in range(h):
+    for x in range(w):
+        flag_row = False
+        flag_column = False
+
+        if x == 0 or s[y][x - 1] == "#":
+            if x == w - 1 or s[y][x + 1] == "#":
+                flag_row = True
+
+        if y == 0 or s[y - 1][x] == "#":
+            if y == h - 1 or s[y + 1][x] == "#":
+                flag_column = True
+
+        if flag_column and flag_row:
+            print(y, x)
