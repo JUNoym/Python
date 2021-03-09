2a853c16a87849183b3e94f6fff7313e346ad75d
junoym <junnyaooyama@gmail.com>
2021-03-09 18:54:05 +0900

    B問題　障害物

--- 3_9.py
+++ 3_9.py
@@ -24,3 +24,43 @@ for _ in range(n):
         y += 1
 
     print(x, y)
+
+# B問題　解けた
+h, w, y, x, m = map(str, input().split())
+h = int(h)
+w = int(w)
+y = int(y)
+x = int(x)
+
+result = "Yes"
+directions = ["N", "E", "S", "W"]
+now_d = 0
+
+s = [list(input()) for _ in range(h)]
+
+if m == 'N':
+    y -= 1
+    if s[y][x] == '#':
+        result = 'No'
+
+
+elif m == 'E':
+    x += 1
+    if s[y][x] == '#':
+        result = 'No'
+
+elif m == 'S':
+    y += 1
+    if s[y][x] == '#':
+        result = 'No'
+
+elif m == 'W':
+    x -= 1
+    if s[y][x] == '#':
+        result = 'No'
+
+
+if x < 0 or x >= int(w) or y < 0 or y >= int(h):
+    result = 'No'
+
+print(result)
