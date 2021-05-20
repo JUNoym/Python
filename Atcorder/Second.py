792824febcde81b0386819fc537372dcda13233b
junoym <junnyaooyama@gmail.com>
2021-05-20 14:58:21 +0900

    ✨ 解答追加B問題20m

--- Atcorder/Second.py
+++ Atcorder/Second.py
@@ -16,3 +16,14 @@ def return_key(d, hei):
 
 
 print(return_key(dict, result))
+
+
+# 解答
+N = int(input())
+data = []
+for i in range(N):
+    S, T = map(str, input().split())
+    T = int(T)
+    data.append([T, S])
+data.sort(reverse=True)
+print(data[1][1])
