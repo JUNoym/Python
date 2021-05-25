d1986ba8c04b878a5bba8537a5664d70637f7935
junoym <junnyaooyama@gmail.com>
2021-05-25 15:25:11 +0900

    ✨ AOJ 解けなかった

---
+++ Atcorder/Missing_card.py
@@ -0,0 +1,13 @@
+pattern = ["S", "H", "C", "D"]
+check = [[False for i in range(13)] for j in range(4)]
+n = int(input())
+
+for i in range(n):
+    e, num = input().split()
+    num = int(num)
+    check[pattern.index(e)][num-1] = True
+
+for i in range(4):  # 0123
+    for j in range(13):  # 0 1 2 3 4 5 6 7 8 9 10 11 12
+        if check[i][j] == False:
+            print(pattern[i], j+1)
