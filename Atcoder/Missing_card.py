827141a0e6300564b295845e6f36f1ef03fd5502
junoym <junnyaooyama@gmail.com>
2021-05-27 00:00:15 +0900

    🐛 typo

--- Atcoder/Missing_card.py
+++ Atcoder/Missing_card.py
@@ -7,6 +7,8 @@ for i in range(n):
     num = int(num)
     check[pattern.index(e)][num-1] = True
 
+print(check)
+
 for i in range(4):  # 0123
     for j in range(13):  # 0 1 2 3 4 5 6 7 8 9 10 11 12
         if check[i][j] == False:
