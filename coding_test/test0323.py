5b570b6dab89b8ac4246077d3317aafb714be432
JUNoym <junnyaooyama@gmail.com>
2022-04-05 20:52:52 +0900

    高速化

--- coding_test/test0323.py
+++ coding_test/test0323.py
@@ -12,3 +12,12 @@ class Solution:
             for j in range(i+1, length):
                 if (nums[i]+nums[j] == target):
                     return [i,j]
+
+# ブルートフォースよくない解き方
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        length = len(nums) #4
+        for i in range(length-1): #0,1,2,3
+            for j in range(i+1, length):
+                if (nums[i]+nums[j] == target):
+                    return [i,j]
