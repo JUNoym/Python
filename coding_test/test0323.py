b45574123ad6216a6205ac4816f610ce42ddcd08
JUNoym <junnyaooyama@gmail.com>
2022-04-06 20:52:52 +0900

    easy

---
+++ coding_test/test0323.py
@@ -0,0 +1,14 @@
+# アンドパッド
+
+import numbers
+
+
+# add two numbers
+# ブルートフォースよくない解き方
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        length = len(nums) #4
+        for i in range(length-1): #0,1,2,3
+            for j in range(i+1, length):
+                if (nums[i]+nums[j] == target):
+                    return [i,j]
