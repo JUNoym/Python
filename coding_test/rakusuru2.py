3ea24ccd08230953e0f6cec90c13bd79bbf04649
JUNoym <junnyaooyama@gmail.com>
2022-04-11 00:55:22 +0900

    修正

--- coding_test/rakusuru2.py
+++ coding_test/rakusuru2.py
@@ -1,7 +1,14 @@
-# n x n の二次元配列を左向きに90°回転させるメソッドのテストを書け
-def rotate(array):
-    n = len(array)
-    for i in range(n // 2):
-        for j in range(i, n - i - 1):
-            array[i][j], array[j][n - i - 1], array[n - i - 1][n - j - 1], array[n - j - 1][i] = array[n - j - 1][i], array[i][j], array[j][n - i - 1], array[n - i - 1][n - j - 1]
-    return array
+# N × N の二次元配列を左向きに90°回転させるメソッドを書く
+# 入力: N
+# 出力: N × N の二次元配列
+
+N = int(input())
+data = [input() for _ in range(N)]
+ans = [[''] * N for _ in range(N)]
+print(data)
+
+for i in range(N):
+    for j in range(N):
+        ans[j][i] = data[i][j]
+for x in ans:
+    print(*x,sep='')
