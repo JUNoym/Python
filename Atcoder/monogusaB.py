e8e6f2a533ed65b17f744657ea20a98a1d3e38f3
JUNoym <junnyaooyama@gmail.com>
2022-05-18 06:43:47 +0900

    B

--- Atcoder/monogusaB.py
+++ Atcoder/monogusaB.py
@@ -3,9 +3,9 @@ ok = True
 if len(list(S)) != len(set(S)): #重複があるかを判定することができる
     ok = False
     
-if not any(s.isupper() for s in S):
+if not any(s.isupper() for s in S): #すべての要素がFalseかどうかを判定
     ok = False
-if not any(s.islower() for s in S):
+if not any(s.islower() for s in S): #すべての要素がFalseかどうかを判定
     ok = False
 if ok:
     print("Yes")
