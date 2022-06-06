b042d295f7e6534155289c66427a5ff3921bf182
JUNoym <junnyaooyama@gmail.com>
2022-06-06 22:05:52 +0900

    AC

--- Atcoder/Contest246C.py
+++ Atcoder/Contest246C.py
@@ -23,3 +23,17 @@ for i in range(K):
         underX[i] = 0
 # print(underX)
 print(sum(underX))
+
+
+# ACのコード
+N, K, X = map(int, input().split())
+A = list(map(int, input().split()))
+
+ans = sum(A)
+rem = K  # クーポンの残り枚数
+Q = sum(x // X for x in A)  # X円引きできる回数
+R = sorted((x % X for x in A), reverse=True)  # X円引きし終わったあとの余りを大きい順に並べる
+ans -= X * min(Q, rem)  # Qとクーポンの枚数の少ない方だけX円引きできる
+rem -= min(Q, rem)  # X円引きに使った分クーポンを減らす
+ans -= sum(R[:rem])  # 大きい順に使う（スライスは配列外参照を起こさないので、remが巨大でも問題ありません）
+print(ans)
